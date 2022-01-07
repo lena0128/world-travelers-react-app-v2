7dbd8c353869e73c84dbe47f9b805b1889e0ed5c
Lena Hou <lenahou2013@gmail.com>
2022-01-07 17:45:28 -0500

    installing react router dom @v6

--- src/components/NewCityForm.js
+++ src/components/NewCityForm.js
@@ -37,29 +37,29 @@ function NewCityForm(){
         <Container className="city-form">
             <Form onSubmit={handleSubmit}>
             <h3>Add a New City</h3>
-                    <Form.Group className="mb-2" controlId="formBasicName">
+                    <Form.Group className="mb-3" controlId="formBasicName">
                         <Form.Label>Name: </Form.Label>
-                        <Form.Control type="text" id="name" name="name" onChange={handleChange} />
+                        <Form.Control size="sm" type="text" id="name" name="name" onChange={handleChange} />
                     </Form.Group>
 
-                    <Form.Group className="mb-2" controlId="formBasicCountry">
+                    <Form.Group className="mb-3" controlId="formBasicCountry">
                         <Form.Label>Country: </Form.Label>
                         <Form.Control type="text" id="country" name="country" onChange={handleChange} />
                     </Form.Group>
 
-                    <Form.Group className="mb-2" controlId="formBasicImage">
+                    <Form.Group className="mb-3" controlId="formBasicImage">
                         <Form.Label>Image: </Form.Label>
                         <Form.Control type="text" id="image" name="image" onChange={handleChange} />
                     </Form.Group>
 
-                    <Form.Group className="mb-2" controlId="formBasicSummary">
+                    <Form.Group className="mb-3" controlId="formBasicSummary">
                         <Form.Label>Summary: </Form.Label>
                         <Form.Control type="text" id="summary" name="summary" onChange={handleChange} />
                     </Form.Group>
 
-                    <Form.Group className="mb-2" controlId="formBasicContent">
+                    <Form.Group className="mb-3" controlId="formBasicContent">
                         <Form.Label>Content: </Form.Label>
-                        <Form.Control type="text" id="content" name="content" onChange={handleChange} />
+                        <Form.Control as="textarea" rows={3} id="content" name="content" onChange={handleChange} />
                     </Form.Group>
                                         
 
